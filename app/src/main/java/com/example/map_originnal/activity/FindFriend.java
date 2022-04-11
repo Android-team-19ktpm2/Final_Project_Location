9f8724f74c398d61881a7eb230e5ca1f1fd5f7c2
phamngocanh19127098 <npham4533@gmail.com>
2022-04-11 11:02:55 +0700

    complete view all friend

---
+++ app/src/main/java/com/example/map_originnal/activity/FindFriend.java
@@ -0,0 +1,140 @@
+package com.example.map_originnal.activity;
+
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.example.map_originnal.R;
+import com.example.map_originnal.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class FindFriend extends AppCompatActivity {
+    DatabaseReference mRef;
+    private ListView listData;
+    ImageView imgSearch;
+    AutoCompleteTextView autoCompleteTextView;
+    FirebaseUser firebaseUser;
+    FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
+        setContentView(R.layout.activity_find_friend);
+        mRef = FirebaseDatabase.getInstance().getReference("Users");
+        listData = (ListView) findViewById(R.id.foundUsers);
+        imgSearch = findViewById(R.id.searchImg);
+        autoCompleteTextView = findViewById(R.id.search_bar);
+
+     //   Log.e("Hello",mRef.getKey()) ;
+        ValueEventListener event = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                populateSearch(snapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        mRef.addListenerForSingleValueEvent(event);
+
+    }
+
+    private void populateSearch(DataSnapshot snapshot) {
+        ArrayList<String> searchedList =  new ArrayList<>();
+        if(snapshot.exists()){
+            for(DataSnapshot ds : snapshot.getChildren()){
+              //  String name = ds.child("id").getValue(String.class);
+               // Log.e("Searched",);
+
+                searchedList.add(String.valueOf(ds.child("email").getValue()));
+            }
+            ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,searchedList);
+            autoCompleteTextView.setAdapter(arrayAdapter);
+            autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String username = autoCompleteTextView.getText().toString();
+                    searchUser(username);
+                }
+
+
+            });
+
+        }
+    }
+
+    private void searchUser(String username) {
+        Query query = mRef.orderByChild("email").equalTo(username);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    ArrayList<String> listUser = new ArrayList<>();
+                    ArrayList<String> listUserID = new ArrayList<>();
+                    for(DataSnapshot ds: snapshot.getChildren()){
+                        //ds.child("id").getValue(String.class)
+                        //                                ,ds.child("email").getValue(String.class)
+                        //                                ,ds.child("avatar").getValue(String.class)
+                        //                                ,ds.child("first_name").getValue(String.class)
+                        //                                ,ds.child("last_name").getValue(String.class)
+                        //                                ,ds.child("online").getValue(String.class)
+                        User user = new User(String.valueOf(ds.child("id").getValue()),
+                                String.valueOf(ds.child("email").getValue()),
+                                String.valueOf(ds.child("avatar").getValue()),
+                                String.valueOf(ds.child("first_name").getValue()),
+                                String.valueOf(ds.child("last_name").getValue()),
+                                String.valueOf(ds.child("online").getValue()));
+                        listUser.add(user.getEmail());
+                        listUserID.add(ds.getKey());
+                    }
+
+                    ArrayAdapter adapter = new ArrayAdapter(getApplicationContext() , android.R.layout.simple_spinner_item,listUser);
+                    listData.setAdapter(adapter);
+                    listData.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            //  Intent intent = new Intent(this,)
+                            Intent intent=new Intent(FindFriend.this,ViewFriend.class);
+                            Log.e("position ",position+"");
+                            intent.putExtra("userID",listUserID.get(position));
+                            startActivity(intent);
+                        }
+                    });
+                }
+                else {
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/map_originnal/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/map_originnal/fragment/ProfileFragment.java
@@ -13,16 +13,18 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.map_originnal.R;
+import com.example.map_originnal.activity.FindFriend;
 import com.example.map_originnal.activity.MainActivity;
 import com.example.map_originnal.activity.StartActivity;
+import com.example.map_originnal.activity.ViewListFriend;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileFragment extends Fragment {
     ImageView profile_image_avatar;
-    ImageButton profile_image_edit,profile_image_favour,profile_image_list,profile_image_logout;
+    ImageButton profile_image_edit,profile_image_favour,profile_image_list,profile_image_logout,findFriend;
     MainActivity main;
-    TextView txtEmail,btn_logout;
+    TextView txtEmail,btn_logout, txtFindFriend,viewAllFriend;
 
 
     FirebaseUser firebaseUser;
@@ -63,14 +65,32 @@ public class ProfileFragment extends Fragment {
         profile_image_logout.setImageResource(R.drawable.logout);
         profile_image_avatar=layout_user.findViewById(R.id.profile_image_avatar);
         profile_image_avatar.setImageResource(R.drawable.avatar);
-
+        findFriend = layout_user.findViewById(R.id.btnFindFriend);
+        txtFindFriend = layout_user.findViewById(R.id.txtFindFriend);
         btn_logout = layout_user.findViewById(R.id.profile_btn_logout);
+        viewAllFriend = layout_user.findViewById(R.id.viewAllFriend);
+        String email = firebaseUser.getEmail();
+        txtEmail.setText(email);
+        txtFindFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), FindFriend.class);
+                startActivity(intent);
+            }
+        });
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 auth.signOut();
                 Intent intent = new Intent(getContext(), StartActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        viewAllFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ViewListFriend.class);
                 startActivity(intent);
             }
         });
