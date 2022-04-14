a6a9935d84f561d18ed73ec5a3782910bb01613e
nguyenson-255 <nguyenson-255@github.com>
2022-04-14 21:53:17 +0700

    build lai mau giao dien

--- Android for ppt/Final_Project_Location-anhPham4/app/src/main/java/com/example/map_originnal/fragment/HomeFragment.java
+++ Android for ppt/Final_Project_Location-anhPham4/app/src/main/java/com/example/map_originnal/fragment/HomeFragment.java
@@ -1,16 +1,24 @@
 package com.example.map_originnal.fragment;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.map_originnal.R;
+import com.example.map_originnal.activity.FindFriend;
+import com.example.map_originnal.activity.ViewListFriend;
 
 public class HomeFragment extends Fragment {
 
+
+    ConstraintLayout txtFindFriend;
+    ImageView viewAllFriend;
     public HomeFragment(){
         // require a empty public constructor
     }
@@ -18,6 +26,26 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        txtFindFriend = v.findViewById(R.id.txtFindFriend);
+        viewAllFriend = v.findViewById(R.id.viewAllFriend);
+
+        viewAllFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ViewListFriend.class);
+                startActivity(intent);
+            }
+        });
+        txtFindFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), FindFriend.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
     }
 }
--- Android for ppt/Final_Project_Location-anhPham4/app/src/main/java/com/example/map_originnal/fragment/ProfileFragment.java
+++ Android for ppt/Final_Project_Location-anhPham4/app/src/main/java/com/example/map_originnal/fragment/ProfileFragment.java
@@ -29,7 +29,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
     ImageView profile_image_avatar;
-    ImageButton profile_image_edit,profile_image_favour,profile_image_logout,findFriend;
+    ImageButton profile_image_edit,profile_image_favour,profile_image_logout;
     MainActivity main;
     TextView txtEmail,btn_logout, txtFindFriend,viewAllFriend,txtName,profile_families;
 
@@ -68,10 +68,7 @@ public class ProfileFragment extends Fragment {
         profile_image_logout.setImageResource(R.drawable.logout);
         profile_image_avatar=layout_user.findViewById(R.id.profile_image_avatar);
         profile_image_avatar.setImageResource(R.drawable.avatar);
-        findFriend = layout_user.findViewById(R.id.btnFindFriend);
-        txtFindFriend = layout_user.findViewById(R.id.txtFindFriend);
         btn_logout = layout_user.findViewById(R.id.profile_btn_logout);
-        viewAllFriend = layout_user.findViewById(R.id.viewAllFriend);
         txtName = layout_user.findViewById(R.id.profile_tv_name);
         profile_families = layout_user.findViewById(R.id.profile_families);
 
@@ -114,27 +111,12 @@ public class ProfileFragment extends Fragment {
                 bottomSheetDialogChangeProfile.show();
             }
         });
-        
-        txtFindFriend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), FindFriend.class);
-                startActivity(intent);
-            }
-        });
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 auth.signOut();
                 Intent intent = new Intent(getContext(), StartActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-        });
-        viewAllFriend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), ViewListFriend.class);
                 startActivity(intent);
             }
         });
