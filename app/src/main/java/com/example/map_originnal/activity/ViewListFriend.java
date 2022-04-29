a784ab875a5de7334b1b7f8907353da22360f2ec
phamngocanh19127098 <npham4533@gmail.com>
2022-04-29 08:53:30 +0700

    add view list request

--- app/src/main/java/com/example/map_originnal/activity/ViewListFriend.java
+++ app/src/main/java/com/example/map_originnal/activity/ViewListFriend.java
@@ -3,7 +3,11 @@ package com.example.map_originnal.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -21,17 +25,21 @@ import java.util.ArrayList;
 public class ViewListFriend extends AppCompatActivity {
     FirebaseAuth mAuth;
     FirebaseUser mUser;
-    DatabaseReference friends;
+    DatabaseReference requests,users;
     ListView lv ;
+    ArrayList<String> searchedList ;
+    String idGuess;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_list_friend);
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
-
-        friends = FirebaseDatabase.getInstance().getReference().child("Friends").child(mUser.getUid());
+        users = FirebaseDatabase.getInstance().getReference().child("Users");
+        requests = FirebaseDatabase.getInstance().getReference().child("Requests");
+        searchedList =  new ArrayList<>();
         lv = findViewById(R.id.lvFriends);
+
         ValueEventListener event = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -43,20 +51,77 @@ public class ViewListFriend extends AppCompatActivity {
 
             }
         };
-        friends.addValueEventListener(event);
-    }
+        ValueEventListener event1 = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ShowFriends(snapshot);
+            }
 
-    private void LoadFriends(DataSnapshot snapshot) {
-        ArrayList<String> searchedList =  new ArrayList<>();
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        requests.addValueEventListener(event);
+        users.addValueEventListener(event1);
+
+
+    }
+    private void ShowFriends(DataSnapshot snapshot){
+        ArrayList<String> searchedListFriend =  new ArrayList<>();
         if(snapshot.exists()) {
             for (DataSnapshot ds : snapshot.getChildren()) {
-                if(String.valueOf(ds.child("status").getValue()).equals("friend")){
-                    searchedList.add(String.valueOf(ds.child("email").getValue()));
+//                Log.e("Test 1",String.valueOf(ds.child(ds.getKey())));
+
+                for(String str:searchedList){
+                    if(str.equals(ds.getKey())){
+                        searchedListFriend.add(String.valueOf(ds.child("email").getValue()));
+                        idGuess = ds.getKey();
+
+                    }
                 }
 
             }
-            ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,searchedList);
+            ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,searchedListFriend);
             lv.setAdapter(arrayAdapter);
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent=new Intent(ViewListFriend.this,ViewFriend.class);
+
+                    Log.e("position ",position+"");
+
+                    intent.putExtra("userID",idGuess);
+                    startActivity(intent);
+                }
+            });
+
+        }
+
+    }
+
+
+    private void LoadFriends(DataSnapshot snapshot) {
+
+        if(snapshot.exists()) {
+            for (DataSnapshot ds : snapshot.getChildren()) {
+//                Log.e("Test 1",String.valueOf(ds.child(ds.getKey())));
+
+                for(DataSnapshot ds1 : ds.getChildren()){
+//                    if(String.valueOf(ds1.getKey()).equals(mUser.getUid())){
+//                        Log.e("Test 1","Request exist");
+//                    }
+                    if(String.valueOf(ds1.getKey()).equals(mUser.getUid())){
+
+                        searchedList.add(ds.getKey());
+                        Log.e("Test 1 ",ds.getKey());
+                    }
+
+
+                }
+
+            }
+
         }
 
     }
